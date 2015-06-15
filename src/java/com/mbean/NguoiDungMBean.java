cb97bcd61ed359e75fecdeda153d6226caeb93fb
Le Thi Minh Loan <Le Thi Minh Loan@ltmloan>
2015-06-15 17:15:13 +0700

    new commit

---
+++ src/java/com/mbean/NguoiDungMBean.java
@@ -0,0 +1,35 @@
+package com.mbean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author Le Thi Minh Loan
+ */
+@ManagedBean
+@SessionScoped
+public class NguoiDungMBean {
+    private String username;
+    private String password;
+    /** Creates a new instance of NguoiDungMBean */
+    public NguoiDungMBean() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
